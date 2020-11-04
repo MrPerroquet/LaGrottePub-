4e0adf7885f33eb505bf73c2c1bb7b675dd50eeb
MrPerroquet <67472321+MrPerroquet@users.noreply.github.com>
2020-11-04 16:06:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client()
 
 //instance
 bot.on('ready', function () {
-	bot.user.setStatus('dnd') 
+	bot.user.setStatus('idle') 
 	bot.user.setActivity('f!help', { type: 'LISTENING'})
 })
 
